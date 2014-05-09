fd812982f44666118be42f6c91cc4c9704dc0515
Brian Rogers <brian.andrew.rogers@gmail.com>
2014-05-09 18:34:01 -0400

    Initial commit

---
+++ RestExample-web/src/main/java/com/mycompany/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.mycompany;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Brian
+ */
+@javax.ws.rs.ApplicationPath("webresources") 
+public class ApplicationConfig extends Application 
+{
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.mycompany.UserResource.class);
+    }
+
+}
