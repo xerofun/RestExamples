363b8f795462afa913ddffa5fc30517e19835f57
Brian Rogers <brian.andrew.rogers@gmail.com>
2014-05-09 20:35:19 -0400

    Added additional RESTful methods

--- RestExample-web/src/main/java/com/mycompany/boundary/UserResource.java
+++ RestExample-web/src/main/java/com/mycompany/boundary/UserResource.java
@@ -1,13 +1,17 @@
 package com.mycompany.boundary;
 
+import com.mycompany.ejb.UserAlreadyExistsException;
+import com.mycompany.ejb.UserNotFoundException;
 import com.mycompany.ejb.UserService;
+import com.mycompany.entities.User;
 import javax.inject.Inject;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -33,27 +37,51 @@ public class UserResource
     {
     }
 
-    /**
-     * Retrieves representation of an instance of com.mycompany.UserResource
-     *
-     * @return an instance of java.lang.String
-     */
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUsers()
     {
         return Response.ok(userService.getAllUsers()).build();
     }
-
-    /**
-     * PUT method for updating or creating an instance of UserResource
-     *
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content)
+    
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("id") long id)
     {
+        Response response;
+        
+        try
+        {
+            response = Response.ok(userService.getUserById(id)).build();
+        }
+        catch (UserNotFoundException ex)
+        {
+            response = Response.status(Response.Status.NOT_FOUND).build();
+        }
+        
+        return response;
+    }
+    
+    
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addUser(User user)
+    {           
+        Response response;
+        
+        try
+        {
+            userService.addUser(user);
+            response = Response.status(Response.Status.CREATED).build();
+        }
+        catch (UserAlreadyExistsException ex)
+        {   // TODO: Log Exception Details
+            response = Response.status(Response.Status.CONFLICT).build();
+        }
+
+        return response;
     }
 }
